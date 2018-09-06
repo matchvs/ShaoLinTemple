55ea689f6f43c73eba2a7da46eecfc49e8ae9477
宋星磊 <823595257@qq.com>
2018-09-06 11:18:56 +0800

    屏幕适配和服务器地址配置

--- library/imports/71/71bef84c-8ce6-4b2b-8910-a8e638c8d999.js
+++ library/imports/71/71bef84c-8ce6-4b2b-8910-a8e638c8d999.js
@@ -27,26 +27,28 @@ cc.Class({
         this.network.chooseNetworkMode();
         this.getRankDataListener();
         this.findPlayerByAccountListener();
-        try {
-            wx.login({
-                success: function success() {
-                    wx.getUserInfo({
-                        fail: function fail(res) {
-                            // iOS 和 Android 对于拒绝授权的回调 errMsg 没有统一，需要做一下兼容处理
-                            if (res.errMsg.indexOf('auth deny') > -1 || res.errMsg.indexOf('auth denied') > -1) {
-                                // 处理用户拒绝授权的情况
-                                console.log("fail");
-                            }
-                        },
-                        success: function success(res) {
-                            Game.GameManager.nickName = res.userInfo.nickName;
-                            Game.GameManager.avatarUrl = res.userInfo.avatarUrl;
-                            console.log('success', Game.GameManager.nickName);
-                        }
-                    });
-                }
-            });
-        } catch (e) {}
+        // try {
+        //     wx.login({
+        //         success: function() {
+        //             wx.getUserInfo({
+        //                 fail: function(res) {
+        //                     // iOS 和 Android 对于拒绝授权的回调 errMsg 没有统一，需要做一下兼容处理
+        //                     if (res.errMsg.indexOf('auth deny') > -1 || res.errMsg.indexOf('auth denied') > -1) {
+        //                         // 处理用户拒绝授权的情况
+        //                         console.log("fail");
+        //                     }
+        //                 },
+        //                 success: function(res) {
+        //                     Game.GameManager.nickName = res.userInfo.nickName;
+        //                     Game.GameManager.avatarUrl = res.userInfo.avatarUrl;
+        //                     console.log('success', Game.GameManager.nickName);
+        //                 }
+        //             });
+        //         }
+        //     })
+        // } catch (e) {
+        //
+        // }
     },
 
 
@@ -113,7 +115,10 @@ cc.Class({
         if (netNotify.userID !== GLB.userInfo.id) {
             GLB.isRoomOwner = true;
         }
-        clientEvent.dispatch(clientEvent.eventType.leaveRoomMedNotify, netNotify);
+        console.log(netNotify);
+        if (netNotify.state === 1) {
+            clientEvent.dispatch(clientEvent.eventType.leaveRoomMedNotify, netNotify);
+        }
     },
 
     kickPlayerNotify: function kickPlayerNotify(_kickPlayerNotify) {
